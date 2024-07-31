ef2618f1c71437172fed6de47e1fc313548accab
RianPham <huynhnhu20050102@gmail.com>
2024-07-31 14:44:50 +0700

    DOM_ mini Project

--- Pokemon_Show/main.js
+++ Pokemon_Show/main.js
@@ -2,7 +2,7 @@
 const contain = document.getElementById("container");
 let baseUrl = "https://raw.githubusercontent.com/GaLaiLapTrinh/pokemon/main/img/";
 
-for( let i=1; i<61; i++)
+for( let i=1; i<=60; i++)
 {
     const newDiv = document.createElement("div");
     const parentDiv = contain.appendChild(newDiv);
